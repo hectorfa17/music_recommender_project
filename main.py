32bd64a81abbca954160b9ae0cb78c0433b2830d
Hector Fontenla Aguilar <hmartin.fa@gmail.com>
2022-05-11 16:59:25 +0200

    name changed for consistency

---
+++ main.py
@@ -0,0 +1,82 @@
+###############################
+# This program lets you       #
+# - Create a dashboard        #
+# - Every dashboard page is  #
+# created in a separate file  #  
+###############################
+
+# Python libraries
+import streamlit as st
+
+# User module files:
+
+from mr import *
+from functions import *
+
+
+def main():
+
+  #st.set_page_config(layout="wide")
+
+#Containers:
+
+  c1 = st.container()
+  c2 = st.container()
+  c3 = st.container()
+  c4 = st.container()
+
+#Song covers:
+
+  cover1 = get_cover('Stone Temple Pilots')
+  cover2 = get_cover('Chris Cornell')
+  cover3 = get_cover('Deep Purple')
+  cover4 = get_cover('Creedence Clearwater Revival')
+  
+    #############  
+    # Main page #
+    #############   
+
+  
+  with c1: 
+    col1, col2, col3 = st.columns(3)
+
+    with col2:
+      st.image('images/logo.png')
+      st.write("## Your music recommender")
+
+    mr()
+                 
+  with c2:
+      
+      st.write('\n\n')
+      st.write('\n\n')
+      st.write('\n\n')
+
+  with c3:
+    col1, col2, col3 = st.columns([2,1,2])
+
+    with col2: 
+      st.write('#### Featured Recommendations:')
+
+      st.write('\n\n')
+      st.write('\n\n')
+      st.write('\n\n')
+
+  with c4: 
+    col1, col2, col3, col4 = st.columns(4)
+
+    with col1:
+
+      st.image(cover1)
+
+    with col2:
+      st.image(cover2)
+
+    with col3:
+      st.image(cover3)
+
+    with col4: 
+      st.image(cover4)
+
+    
+main()
